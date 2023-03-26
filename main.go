19d33fcd680e1a1b020815984b8b0b6ed88e27ad
Beknazar Abdyldaev <abdyldaevgroup@gmail.com>
2023-03-26 17:27:25 +0600

    realize crud

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-	ID       int    `json:"id"`
+	ID       int64  `json:"id"`
 	UserName string `json:"username"`
 	Password string `json:"password"`
 }
@@ -22,8 +24,73 @@ func getUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, Users)
 }
 
+func addUser(c *gin.Context) {
+	var newUser User
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+	Users = append(Users, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func getUserById(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	fmt.Println("id ")
+	if err != nil {
+		return
+	}
+	for _, user := range Users {
+		if user.ID == id {
+			c.IndentedJSON(http.StatusOK, user)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"detail": "User not found"})
+}
+
+func deleteUserById(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	fmt.Println("id ")
+	if err != nil {
+		return
+	}
+	for _, user := range Users {
+		if user.ID == id {
+			Users = append(Users[:id], Users[id+1:]...)
+			c.IndentedJSON(http.StatusOK, user)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"detail": "User not found"})
+}
+
+func UpdateUserById(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	fmt.Println("id ")
+	if err != nil {
+		return
+	}
+	var UpdateUser User
+	if err := c.BindJSON(&UpdateUser); err != nil {
+		return
+	}
+	for i := 0; i < len(Users); i++ {
+		if Users[i].ID == id {
+			Users[i] = UpdateUser
+			c.IndentedJSON(http.StatusOK, Users[i])
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"detail": "User not found"})
+}
+
 func main() {
+
 	router := gin.Default()
-	router.GET("/users", getUsers)
+	router.GET("/user", getUsers)
+	router.GET("/user/:id", getUserById)
+	router.POST("/user", addUser)
+	router.DELETE("/user/:id", deleteUserById)
+	router.PUT("/user/:id", UpdateUserById)
 	router.Run("0.0.0.0:8000")
 }
